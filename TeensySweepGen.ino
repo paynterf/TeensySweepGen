87cdb24806a1902c775632ffac6dff6819c41018
paynterf <paynterf@gmail.com>
2017-08-01 18:44:29 -0400

    08/01/17 ready to publish

--- TeensySweepGen.ino
+++ TeensySweepGen.ino
@@ -1,16 +1,22 @@
-//Frequency and amplitude sweep tool for testing IR demodulator
-//07/11/17 modified to trigger the demodulator so its output can be related to sweep freq
+//Frequency and amplitude sweep tool for testing frequency and/or amplitude sensitive devices
+//Output is square wave frequency sweep at specified output level (peak-peak % of full-scale DAC output),
+//or p-p amplitude sweep at specified frequency.  The output appears on Teensy pin A21 (DAC0).
+//the output square wave is echoed to Teensy digital output pin 33 for frequency monitoring
+//Teensy digital output pin 1 is toggled HIGH at the start, and LOW at the end of each sweep
+//Measurement input is expected on analog input pin A12.  This pin is read once at the end of each sweep step
+//Latest source can be found on GitHub at 
+
 #include <ADC.h> //Analog-to-Digital library
 
 ADC *adc = new ADC(); // adc object;
 IntervalTimer myTimer; //added 07/23/17
 
 
-const int SQWAVE_PIN = 33;
-const int DAC_PIN = A21; //DAC0
+const int SQWAVE_PIN = 33; //monitor output
+const int DAC_PIN = A21; //DAC0: freq/amp sweep output pin
+const int DAC_OUT_HIGH_VAL = 4096;
 const int LED_PIN = 13; //added 03/10/15
-const int DEMOD_SYNCH_OUT_PIN = 1;
-const int DEMOD_SYNCH_IN_PIN = 2;
+const int DEMOD_SYNCH_OUT_PIN = 1; //goes HIGH at start and LOW at end of each sweep
 const int DEMOD_VALUE_READ_PIN = A12;
 
 const int DEFAULT_START_FREQ = 510;//8Hz below
@@ -58,7 +64,6 @@ void setup()
 	pinMode(LED_PIN, OUTPUT);
 	pinMode(DAC_PIN, OUTPUT); //analog output pin
 	pinMode(DEMOD_SYNCH_OUT_PIN, OUTPUT); //07/11/17 added for triggering IR demod modle
-	pinMode(DEMOD_SYNCH_IN_PIN, INPUT_PULLDOWN); //07/11/17 added for triggering IR demod modle
 	pinMode(DEMOD_VALUE_READ_PIN, INPUT); //analog input from demod module
 
 	digitalWrite(SQWAVE_PIN, LOW);
@@ -78,183 +83,204 @@ void setup()
 	adc->setResolution(12, ADC_1); // set bits of resolution
 	adc->setConversionSpeed(ADC_CONVERSION_SPEED::MED_SPEED, ADC_1); // change the conversion speed
 	adc->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED, ADC_1); // change the sampling speed
-	//adc->setAveraging(16, ADC_1); // set number of averages
-	//adc->setResolution(12, ADC_1); // set bits of resolution
-	//adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_1); // change the conversion speed
-	//adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_1); // change the sampling speed
 
 
 
-
-	Serial.print("Choose Freq or Amp Sweep.  F/A (F)? ");
-	while (Serial.available() == 0); //waits for input
-	String res = Serial.readString().trim();
-	Serial.println(res);
-	if (!res.equalsIgnoreCase('A'))
+	while (1) //infinite loop.  User can exit at bottom
 	{
-		////Get Sweep parameters
-		FreqStart = GetIntegerParameter("Start Freq?", DEFAULT_START_FREQ);
-		FreqEnd = GetIntegerParameter("End Freq?", DEFAULT_END_FREQ);
-		FreqSteps = GetIntegerParameter("Freq Steps?", DEFAULT_FREQ_STEPS);
-		SecPerFreqStep = GetFloatParameter("Seconds Per Step?", DEFAULT_SEC_PER_FREQ_STEP);
-		OutLevelPct = GetIntegerParameter("Output Level Pct?", DEFAULT_OUTPUT_PCT);
-		//FreqStart = DEFAULT_START_FREQ;
-		//FreqEnd = DEFAULT_END_FREQ;
-		//FreqSteps = DEFAULT_FREQ_STEPS;
-		//SecPerFreqStep = DEFAULT_SEC_PER_FREQ_STEP;
-		//OutLevelPct = DEFAULT_OUTPUT_PCT;
-
-		Serial.print("Sweep Parameters are: "); 
-		Serial.print("Start = "); Serial.print(FreqStart);
-		Serial.print(", End = "); Serial.print(FreqEnd);
-		Serial.print(", Steps = "); Serial.print(FreqSteps);
-		Serial.println();
-		Serial.print("SecPerFreqStep = "); Serial.print(SecPerFreqStep);
-		Serial.print(", OutputLevel = "); Serial.print(OutLevelPct);
-		Serial.println();
-
-		Serial.print("Start Y/N (Y)? ");
-		while (Serial.available() == 0); //waits for input
+		Serial.print("Choose Freq or Amp Sweep.  F/A (F)? ");
+		while (Serial.available() == 0)
+		{
+			delay(100); //waits for input
+		}
 		String res = Serial.readString().trim();
 		Serial.println(res);
-		if (!res.equalsIgnoreCase('N'))
+		if (!res.equalsIgnoreCase('A'))
 		{
-			Serial.print("Sent trigger to pin "); Serial.println(DEMOD_SYNCH_OUT_PIN);
-			digitalWrite(DEMOD_SYNCH_OUT_PIN, HIGH); //trigger IR demod start
+			////Get Sweep parameters
+			FreqStart = GetIntegerParameter("Start Freq?", DEFAULT_START_FREQ);
+			FreqEnd = GetIntegerParameter("End Freq?", DEFAULT_END_FREQ);
+			FreqSteps = GetIntegerParameter("Freq Steps?", DEFAULT_FREQ_STEPS);
+			SecPerFreqStep = GetFloatParameter("Seconds Per Step?", DEFAULT_SEC_PER_FREQ_STEP);
+			OutLevelPct = GetIntegerParameter("Output Level Pct?", DEFAULT_OUTPUT_PCT);
+			//FreqStart = DEFAULT_START_FREQ;
+			//FreqEnd = DEFAULT_END_FREQ;
+			//FreqSteps = DEFAULT_FREQ_STEPS;
+			//SecPerFreqStep = DEFAULT_SEC_PER_FREQ_STEP;
+			//OutLevelPct = DEFAULT_OUTPUT_PCT;
 
-			float freqstepHz = (float)(FreqEnd - FreqStart) / (float)(FreqSteps);
-			DACoutHigh = 4096;
-			DACoutLow = DACoutHigh*OutLevelPct / 100;
-			//07/11/17 now doing only one sweep, but it is synched with demod
-			//Serial.print("Iteration # "); Serial.println(i + 1);
-			Serial.println("Starting....");
-			Serial.println("Step\tFreq\tValue");
+			Serial.print("Sweep Parameters are: ");
+			Serial.print("Start = "); Serial.print(FreqStart);
+			Serial.print(", End = "); Serial.print(FreqEnd);
+			Serial.print(", Steps = "); Serial.print(FreqSteps);
+			Serial.println();
+			Serial.print("SecPerFreqStep = "); Serial.print(SecPerFreqStep);
+			Serial.print(", OutputLevel = "); Serial.print(OutLevelPct);
+			Serial.println();
 
-			for (int i = 0; i <= FreqSteps; i++)
+			Serial.print("Start Y/N (Y)? ");
+			while (Serial.available() == 0)
 			{
-				float freqHz = FreqStart + i*freqstepHz;
-				//Serial.print("Step "); Serial.print(i + 1); Serial.print(" Freq = "); Serial.println(freqHz);
-
-				//compute required elapsed time for square wave transitions
-				HalfCycleMicroSec = 0.5e6 / freqHz;
-
-				//07/30/17 now using tni's technique for updating timer w/o reset 
-				if (i == 0)
-				{
-					myTimer.begin(SqwvGen, HalfCycleMicroSec);
-				}
-				else
-				{
-					updateInterval(myTimer, HalfCycleMicroSec);
-				}
-
-				delay(SecPerFreqStep*1000);
-
-				//read & print the analog voltage
-				int FinalVal = adc->analogRead(DEMOD_VALUE_READ_PIN); //0-4096
-				Serial.print(i+1); Serial.print("\t");
-				Serial.print(freqHz); Serial.print("\t");
-				Serial.print(FinalVal);
-				Serial.println();
+				delay(100); //waits for input
 			}
-				myTimer.end(); //07/29/17 moved outside of freq step loop
-		}
-	}
-	else//must be amplitude sweep
-	{
-		//AmpStartPct = GetIntegerParameter("Start Amp Pct?", DEFAULT_START_AMP_PCT);
-		//AmpEndPct = GetIntegerParameter("End Amp Pct?", DEFAULT_END_AMP_PCT);
-		//AmpSteps = GetIntegerParameter("Amp Steps?", DEFAULT_AMP_STEPS);
-		//SecPerAmpStep = GetFloatParameter("Sec Per Step?", DEFAULT_SEC_PER_AMP_STEP);
-		//AmpCtrFreqHz = GetFloatParameter("Output Ctr Freq HZ?", DEFAULT_AMP_CTR_FREQ_HZ);
-		AmpStartPct = DEFAULT_START_AMP_PCT;
-		AmpEndPct = DEFAULT_END_AMP_PCT;
-		AmpSteps = DEFAULT_AMP_STEPS;
-		SecPerAmpStep = DEFAULT_SEC_PER_AMP_STEP;
-		AmpCtrFreqHz = DEFAULT_AMP_CTR_FREQ_HZ;
 
-		Serial.print("Sweep Parameters are: ");
-		Serial.print("Start = "); Serial.print(AmpStartPct);
-		Serial.print(", End = "); Serial.print(AmpEndPct);
-		Serial.print(", Steps = "); Serial.print(AmpSteps);
-		Serial.println();
-		Serial.print("SecPerAmpStep = "); Serial.print(SecPerAmpStep);
-		Serial.print(", Output Ctr Freq = "); Serial.print(AmpCtrFreqHz);
-		Serial.println();
-
-		//Serial.print("Start Y/N (Y)? ");
-		//while (Serial.available() == 0); //waits for input
-		//String res = Serial.readString().trim();
-		//Serial.println(res);
-		//if (!res.equalsIgnoreCase('N'))
-		{
-			Serial.println("Triggering IR Demod ....");
-			digitalWrite(DEMOD_SYNCH_OUT_PIN, HIGH); //trigger IR demod start
-													 //while (digitalRead(DEMOD_SYNCH_IN_PIN) == LOW)
-													 //{
-													 //	//infinite loop to wait for trigger from IR demod module
-													 //	Serial.println("Waiting for IR Demod Response....");
-													 //	delay(1000);
-													 //}
-
-			//float AmpStepPct = (float)(AmpEndPct - AmpStartPct) / (float)(AmpSteps - 1);
-			float AmpStepPct = (AmpSteps > 1)? (float)(AmpEndPct - AmpStartPct) / (float)(AmpSteps - 1): 0;
-			DACoutHigh = 4096;
-			//int DACoutLow = 0;
-
-			//07/11/17 now doing only one sweep, but it is synched with demod
-			//for (int i = 0; i < 10; i++)
+			String res = Serial.readString().trim();
+			Serial.println(res);
+			if (!res.equalsIgnoreCase('N'))
 			{
-				//compute required elapsed time for square wave transitions
-				HalfCycleMicroSec = 0.5e6 / AmpCtrFreqHz;
-				Serial.print("HalfCycleMicroSec = "); Serial.println(HalfCycleMicroSec);
-				Serial.print("SecPerAmpStep = "); Serial.println(SecPerAmpStep);
-
-				//Serial.print("Iteration # "); Serial.println(i + 1);
-				Serial.println("Starting....");
-				Serial.println("Step\tAmpPct\tValue");
-
-				for (int i = 0; i < AmpSteps; i++)
+				Serial.println("Send any key to stop sweep repeat");
+				while (Serial.available() == 0) //do sweeps until user quits
 				{
-					float AmpPct = AmpStartPct + i*AmpStepPct;
-					//Serial.print("Step "); Serial.print(i + 1); Serial.print(" Amp = "); Serial.println(AmpPct);
+					Serial.print("Sent trigger to pin "); Serial.println(DEMOD_SYNCH_OUT_PIN);
+					digitalWrite(DEMOD_SYNCH_OUT_PIN, HIGH); //trigger IR demod start
 
-					//output a square wave for the specified number of seconds
-					long startMsec = millis();
-					//while (stopMsec - startMsec < 1000 * SecPerAmpStep)
-					while (millis() - startMsec < 1000 * SecPerAmpStep)
+					float freqstepHz = (float)(FreqEnd - FreqStart) / (float)(FreqSteps);
+					DACoutHigh = DAC_OUT_HIGH_VAL;
+					DACoutLow = DACoutHigh - DACoutHigh * OutLevelPct / 100; //can't comb terms due to int arith prob
+
+					Serial.println("Starting....");
+					Serial.println("Step\tFreq\tValue");
+
+					for (int i = 0; i <= FreqSteps; i++)
 					{
-						long startUsec = micros();
-						digitalWrite(SQWAVE_PIN, HIGH);
-						analogWriteDAC0(DACoutHigh); 
-						digitalWrite(LED_PIN, HIGH);
-						while (micros() - startUsec < HalfCycleMicroSec);//wait for rest of half-period to elapse
+						float freqHz = FreqStart + i*freqstepHz;
 
-						digitalWrite(SQWAVE_PIN, LOW);
-						analogWriteDAC0(DACoutHigh * (1- AmpPct/100) ); //level for this amp step
-						digitalWrite(LED_PIN, LOW);
-						while (micros() - startUsec < 2 * HalfCycleMicroSec);//wait for rest of half-period to elapse
+						//compute required elapsed time for square wave transitions
+						HalfCycleMicroSec = 0.5e6 / freqHz;
+
+						//07/30/17 now using tni's technique for updating timer w/o reset 
+						if (i == 0)
+						{
+							myTimer.begin(SqwvGen, HalfCycleMicroSec); //SqwvGen is name of ISR
+						}
+						else
+						{
+							updateInterval(myTimer, HalfCycleMicroSec);
+						}
+
+						delay(SecPerFreqStep * 1000);
+
+						//read & print the analog voltage
+						int FinalVal = adc->analogRead(DEMOD_VALUE_READ_PIN); //0-4096
+						Serial.print(i + 1); Serial.print("\t");
+						Serial.print(freqHz); Serial.print("\t");
+						Serial.print(FinalVal);
+						Serial.println();
+					}
+					myTimer.end(); //07/29/17 moved outside of freq step loop
+
+					//toggle synch line
+					digitalWrite(DEMOD_SYNCH_OUT_PIN, LOW);
+					Serial.print("Disabled trigger on pin "); Serial.println(DEMOD_SYNCH_OUT_PIN);
+				}
+			}
+		}
+		else//must be amplitude sweep
+		{
+			AmpStartPct = GetIntegerParameter("Start Amp Pct?", DEFAULT_START_AMP_PCT);
+			AmpEndPct = GetIntegerParameter("End Amp Pct?", DEFAULT_END_AMP_PCT);
+			AmpSteps = GetIntegerParameter("Amp Steps?", DEFAULT_AMP_STEPS);
+			SecPerAmpStep = GetFloatParameter("Sec Per Step?", DEFAULT_SEC_PER_AMP_STEP);
+			AmpCtrFreqHz = GetFloatParameter("Output Ctr Freq HZ?", DEFAULT_AMP_CTR_FREQ_HZ);
+			//AmpStartPct = DEFAULT_START_AMP_PCT;
+			//AmpEndPct = DEFAULT_END_AMP_PCT;
+			//AmpSteps = DEFAULT_AMP_STEPS;
+			//SecPerAmpStep = DEFAULT_SEC_PER_AMP_STEP;
+			//AmpCtrFreqHz = DEFAULT_AMP_CTR_FREQ_HZ;
+
+			Serial.print("Sweep Parameters are: ");
+			Serial.print("Start = "); Serial.print(AmpStartPct);
+			Serial.print(", End = "); Serial.print(AmpEndPct);
+			Serial.print(", Steps = "); Serial.print(AmpSteps);
+			Serial.println();
+			Serial.print("SecPerAmpStep = "); Serial.print(SecPerAmpStep);
+			Serial.print(", Output Ctr Freq = "); Serial.print(AmpCtrFreqHz);
+			Serial.println();
+
+			Serial.print("Start Y/N (Y)? ");
+			while (Serial.available() == 0)
+			{
+				delay(100); //waits for input
+			}
+
+			String res = Serial.readString().trim();
+			Serial.println(res);
+			if (!res.equalsIgnoreCase('N'))
+			{
+				Serial.println("Send any key to stop sweep repeat");
+				while (Serial.available() == 0) //do sweeps until user quits
+				{
+					Serial.print("Sent trigger to pin "); Serial.println(DEMOD_SYNCH_OUT_PIN);
+					digitalWrite(DEMOD_SYNCH_OUT_PIN, HIGH); //trigger IR demod start
+
+					float AmpStepPct = (AmpSteps > 1) ? (float)(AmpEndPct - AmpStartPct) / (float)(AmpSteps - 1) : 0;
+					DACoutHigh = DAC_OUT_HIGH_VAL;
+
+					//compute required elapsed time for square wave transitions
+					HalfCycleMicroSec = 0.5e6 / AmpCtrFreqHz;
+					Serial.print("HalfCycleMicroSec = "); Serial.println(HalfCycleMicroSec);
+					Serial.print("SecPerAmpStep = "); Serial.println(SecPerAmpStep);
+
+					Serial.println("Starting....");
+					Serial.println("Step\tAmpPct\tValue");
+
+					for (int i = 0; i < AmpSteps; i++)
+					{
+						float AmpPct = AmpStartPct + i*AmpStepPct;
+						//Serial.print("Step "); Serial.print(i + 1); Serial.print(" Amp = "); Serial.println(AmpPct);
+
+						//output a square wave for the specified number of seconds
+						long startMsec = millis();
+						while (millis() - startMsec < 1000 * SecPerAmpStep)
+						{
+							long startUsec = micros();
+							digitalWrite(SQWAVE_PIN, HIGH);
+							analogWriteDAC0(DACoutHigh);
+							digitalWrite(LED_PIN, HIGH);
+							while (micros() - startUsec < HalfCycleMicroSec);//wait for rest of half-period to elapse
+
+							digitalWrite(SQWAVE_PIN, LOW);
+							analogWriteDAC0(DACoutHigh * (1 - AmpPct / 100)); //level for this amp step
+							digitalWrite(LED_PIN, LOW);
+							while (micros() - startUsec < 2 * HalfCycleMicroSec);//wait for rest of half-period to elapse
+						}
+
+						//read & print the analog voltage
+						int FinalVal = adc->analogRead(DEMOD_VALUE_READ_PIN); //0-4096
+						Serial.print(i + 1); Serial.print("\t");
+						Serial.print(AmpPct); Serial.print("\t");
+						Serial.print(FinalVal);
+						Serial.println();
 					}
 
-					//read & print the analog voltage
-					int FinalVal = adc->analogRead(DEMOD_VALUE_READ_PIN); //0-4096
-					Serial.print(i + 1); Serial.print("\t");
-					Serial.print(AmpPct); Serial.print("\t");
-					Serial.print(FinalVal);
-					Serial.println();
-
-
-					//delay(1000);
+					//toggle synch line
+					digitalWrite(DEMOD_SYNCH_OUT_PIN, LOW);
+					Serial.print("Disabled trigger on pin "); Serial.println(DEMOD_SYNCH_OUT_PIN);
 				}
 			}
-			delay(3000);
 		}
 
-	}
+		//clear the receive buffer
+		while (Serial.available() != 0)
+		{
+			char getData = Serial.read();
+			getData++; //just to avoid 'unused symbol' warning
+		}
 
-	Serial.println("Exiting - Bye!");
-	digitalWrite(DEMOD_SYNCH_OUT_PIN, LOW);
-	while (1);
+		Serial.print("Do Again Y/N (Y)? ");
+		while (Serial.available() == 0)
+		{
+			delay(100); //waits for input
+		}
+
+		res = Serial.readString().trim();
+		Serial.println(res);
+		if (res.equalsIgnoreCase('N'))
+		{
+			Serial.println("Exiting - Bye!");
+			while (1);
+		}
+	}
 }//setup()
 
 // functions called by IntervalTimer should be short, run as quickly as
